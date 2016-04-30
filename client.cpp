8eb933a7d5d55b377cabf82fb477711ef8ae1a55
Taffy Cheng <taffy128s@gmail.com>
2016-04-30 10:26:42 +0800

    Basic functions added.

--- client.cpp
+++ client.cpp
@@ -1,14 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/stat.h>
+#define MAX 2048
 
-int main() {
+int main(int argc, char **argv) {
+	int sockfd;
+	struct sockaddr_in servaddr;
+	if (argc != 2) {
+		fprintf(stderr, "usage: ./client <IPaddress>\n");
+		exit(1);
+	}
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(6666);
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	
 	return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,14 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/stat.h>
+#define MAX 2048
 
 int main() {
-	int test;
+	int sockfd;
+	struct sockaddr_in servaddr, cliaddr;
+	
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(6666);
+	
+	bind(sockfd, (struct sockaddr *) &servaddr, sizeof (servaddr));
+	
 	return 0;
 }
